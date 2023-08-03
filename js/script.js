5e202e7bd29cffaa75bbb58c08a5e524760277dd
Jhonny <mjhonnyvieira@gmail.com>
2023-08-03 19:32:23 -0300

    Recomendations

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let idade = prompt("Quantos aninhos?")
+
+if(idade < 18 ) {
+    alert("Nem devia entrar aki... Te esperam traumas!")
+}
+else if(idade > 17 && idade <59) {
+    alert("Recomendo que volte. A curiosidade matou o gato")
+    alert("Você foi avisado!")
+}
+else {
+    alert("Vai ter um infarto...")
+    alert("R.I.P")
+}
